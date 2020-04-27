071f13098731f95a8666802ec6b191ea673f7a9c
priyanko-das <priyanko.das@publicissapient.com>
2020-04-27 20:44:39 +0530

    checking again

--- server/index.js
+++ server/index.js
@@ -14,10 +14,12 @@ const app=express();
 const file=fs.readFileSync('dist/index.html').toString();
 const part1=file.split("<form>");
 const part2=part1.toString().split("</form>");
+const part1divide=part1.split('<div id="root">');
 app.use('/',express.static("dist"));
 app.use((req,res)=>{
     
-    res.write(part1[0]+"just confirming");
+    res.write(part1divide[0]+"just confirming"+part1divide[1]);
+    //res.write(part1[0]);
     const reactMarkUp=(
     <StaticRouter url={req.url}>
         <Provider store={store}>
